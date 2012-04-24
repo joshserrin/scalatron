7875b49797ce92e3d312ad39f00cf3b3f1723bba
Josh Serrin <jserrin@cra.com>
2012-04-24 10:48:39 -0400

    when publishing just run the app right after

--- publish.sh
+++ publish.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 SCALATRON=/home/jserrin/projects/scalatron/scalatron-0.9.3/Scalatron
 DEST=$SCALATRON/bots/JoshSerrin
 mkdir $DEST
@@ -5,3 +7,8 @@ mkdir $DEST
 sbt package 
 
 cp target/scala-2.9.1/cra_scalatron-jls*.jar $DEST/ScalatronBot.jar
+
+# Run the app!
+pushd /home/jserrin/projects/scalatron/scalatron-0.9.3/Scalatron/bin
+./Scalatron.jar
+popd
